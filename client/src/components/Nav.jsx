a1f28f23a4bf3aefefc6e50b06cb141a367b53a8
0103lys <jkssy1@naver.com>
2022-01-29 04:10:22 +0900

    220129 추가내용

---
+++ client/src/components/Nav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Nav(){
+    return(
+        <nav>
+            <ul>
+                <li>회원가입</li>
+                <li>로그인</li>
+            </ul>
+        </nav>
+
+    )
+}
+
+export default Nav;
